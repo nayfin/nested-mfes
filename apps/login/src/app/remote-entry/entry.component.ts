efb7ceb514c53398ff69959f077cb738dda2e396
Nathan Kern <NathanKern@fico.com>
2022-11-15 10:27:21 -0600

    generates host/remote mfes and data-access service

---
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nested-mfes-login-entry',
+  template: `<nested-mfes-nx-welcome></nested-mfes-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
