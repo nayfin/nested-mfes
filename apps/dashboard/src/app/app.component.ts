64dae98a588c19599cac2fd21bb22e47fc488a0e
Nathan Kern <NathanKern@fico.com>
2022-11-15 11:36:58 -0600

    adds login logic and conditional shows login mfe when logged out

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '@nested-mfes/shared/data-access-user';
+import { distinctUntilChanged } from 'rxjs/operators';
 @Component({
   selector: 'nested-mfes-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+  constructor(private userService: UserService, private router: Router) {}
+  ngOnInit() {
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        console.log({loggedIn})
+        // Queue the navigation after initialNavigation blocking is completed
+        setTimeout(() => {
+          if (!loggedIn) {
+            this.router.navigateByUrl('login');
+          } else {
+            this.router.navigateByUrl('');
+          }
+        });
+      });
+  }
 }
--- apps/login/src/app/remote-entry/entry.component.ts
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { UserService } from '@nested-mfes/shared/data-access-user';
 
 @Component({
   selector: 'nested-mfes-login-entry',
-  template: `<nested-mfes-nx-welcome></nested-mfes-nx-welcome>`,
+  template: `  
+  LOGIN REMOTE
+  <div class="login-app">
+    <form class="login-form" (ngSubmit)="login()">
+      <label>
+        Username:
+        <input type="text" name="username" [(ngModel)]="username" />
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" [(ngModel)]="password" />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+    <div *ngIf="isLoggedIn$ | async">User is logged in!</div>
+  </div>
+  `,
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent {
+  username = '';
+  password = '';
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+  constructor(private userService: UserService) {}
+  login() {
+    this.userService.checkCredentials(this.username, this.password);
+  }
+}
