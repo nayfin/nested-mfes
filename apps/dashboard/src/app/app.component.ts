5082ca3d0be8d2fa9e41964725909399f125be5d
Nathan Kern <NathanKern@fico.com>
2022-11-16 13:57:19 -0600

    renders message mfe as component using vc ref

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '@nested-mfes/shared/data-access-user';
 import { distinctUntilChanged } from 'rxjs/operators';
@@ -7,14 +7,22 @@ import { distinctUntilChanged } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  
+  @ViewChild('mfeOutlet', { read: ViewContainerRef })
+  viewContainer!: ViewContainerRef;
+
   isLoggedIn$ = this.userService.isUserLoggedIn$;
-  constructor(private userService: UserService, private router: Router) {}
+
+  constructor(
+    private userService: UserService, 
+    private router: Router
+  ) {}
+
   ngOnInit() {
     this.isLoggedIn$
       .pipe(distinctUntilChanged())
       .subscribe(async (loggedIn) => {
-        console.log({loggedIn})
         // Queue the navigation after initialNavigation blocking is completed
         setTimeout(() => {
           if (!loggedIn) {
@@ -24,5 +32,10 @@ export class AppComponent implements OnInit {
           }
         });
       });
+  }
+
+  async ngAfterViewInit() {
+    const m = await import('message/Component');
+    const ref = this.viewContainer.createComponent(m.RemoteEntryComponent);
   }
 }
