70c6cf4aea3010049121eea80e80efcd084f3196
Nathan Kern <NathanKern@fico.com>
2022-11-29 11:12:36 -0600

    enables child MFE to parent MFE communication

--- apps/login/src/app/remote-entry/entry.component.ts
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -1,10 +1,14 @@
 import { Component, ComponentRef, OnInit } from '@angular/core';
 import { UserService } from '@nested-mfes/shared/data-access-user';
+import { Subject, tap } from 'rxjs';
 
 @Component({
   selector: 'nested-mfes-login-entry',
+  styleUrls: ['./entry.component.scss'],
   template: `  
-  LOGIN REMOTE
+  <h2>
+    LOGIN (REMOTE MFE)
+  </h2>
   <div class="login-app">
     
     <ng-container 
@@ -16,7 +20,7 @@ import { UserService } from '@nested-mfes/shared/data-access-user';
       [initializationCallback]="initializeMfe"
     ></ng-container>
 
-    <form class="login-form" (ngSubmit)="login()">
+    <!-- <form class="login-form" (ngSubmit)="login()">
       <label>
         Username:
         <input type="text" name="username" [(ngModel)]="username" />
@@ -27,28 +31,40 @@ import { UserService } from '@nested-mfes/shared/data-access-user';
       </label>
       <button type="submit">Login</button>
     </form>
-    <div *ngIf="isLoggedIn$ | async">User is logged in!</div>
+    <div *ngIf="isLoggedIn$ | async">User is logged in!</div> -->
   </div>
   `,
 })
 export class RemoteEntryComponent implements OnInit {
 
   remoteDefinitions: undefined | Record<string, string>;
+  mfeRef: undefined | ComponentRef<unknown>;
 
   username = '';
   password = '';
   isLoggedIn$ = this.userService.isUserLoggedIn$;
+
   constructor(private userService: UserService) {}
+  
   login() {
     this.userService.checkCredentials(this.username, this.password);
   }
 
-  initializeMfe(ref: ComponentRef<unknown>) {
-    ref.setInput('message', 'Howdy');
+  initializeMfe(ref: ComponentRef<any>) {
+    // Demonstrates setting an input of the MFE
+    ref.setInput('message', 'Updated Message');
+    // Demonstrates listening for events from the MFE
+    this.mfeRef = ref;
+    (ref.instance.output$ as Subject<string>).pipe(
+      tap(console.log)
+    ).subscribe();
+    // TODO: unsubscribe onDestroy
+    // ref.onDestroy
   }
 
   async ngOnInit () {
-    // TODO: this shouldn't be needed here and could potentially overwrite existing remote definitions 
+    // get the remote definitions so that they can be passed to generated MFE
+    // TODO: this may not be needed here and could potentially overwrite existing remote definitions 
     await fetch('/assets/module-federation.manifest.json')
       .then((res) => res.json())
       .then((definitions) => {
--- apps/message/src/app/remote-entry/entry.component.ts
+++ apps/message/src/app/remote-entry/entry.component.ts
@@ -1,15 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'nested-mfes-message-entry',
   standalone: true,
+  styleUrls: ['./entry.component.scss'],
   template: `    
-    <h1>
-      MESSAGE COMPONENT
-    </h1>
+    <h3>
+      MESSAGE COMPONENT (Nested Remote MFE)
+    </h3>
     <h2>Message: {{message}}</h2>
+    <button (click)="output$.next('HOWDY')">Send Event To Parent</button>
   `,
 })
 export class RemoteEntryComponent {
   @Input() message = 'Initial Message';
+
+  output$ = new Subject<string>();
 }
