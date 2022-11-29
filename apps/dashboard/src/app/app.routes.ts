eb71bf326b090dd55b1b3fd22df84a138d6e9669
Nathan Kern <NathanKern@fico.com>
2022-11-29 11:13:00 -0600

    Cleans up demo to make it easier to see relationship

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     loadChildren: () => loadRemoteModule('login', './Module').then((m) => m.RemoteEntryModule)
   },
   {
-    path: '',
+    path: 'welcome',
     component: NxWelcomeComponent,
   },
 ];
